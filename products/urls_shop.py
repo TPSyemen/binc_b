a8125203435b3b421e82bf06787c59018399471a
TPSymene <ttt.ppp.sss.77@gmail.com>
2025-06-22 19:50:17 +0300

    "

--- products/urls_shop.py
+++ products/urls_shop.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .views_shop import ShopViewSet, ShopRegisterView
+from .views_shop import ShopViewSet, ShopRegisterView, ShopCheckView
 
 router = DefaultRouter()
 router.register(r'', ShopViewSet, basename='shop')
 
 urlpatterns = [
     path('create/', ShopRegisterView.as_view(), name='shop-create'),
+    path('check/', ShopCheckView.as_view(), name='shop-check'),
 ]
 
 urlpatterns += router.urls
