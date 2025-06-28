9d7d8ffed66e04deb37f482e021da770fede3cd8
Mugahed Ahmed Batahan <ttt.ppp.sss.77@gmail.com>
2025-06-28 13:03:32 +0300

    ة

--- products/serializers.py
+++ products/serializers.py
@@ -49,148 +49,13 @@ class ProductListSerializer(serializers.ModelSerializer):
     category = CategorySerializer(read_only=True)
     discount = serializers.SerializerMethodField()
     rating = serializers.FloatField(read_only=True)
-    in_stock = serializers.BooleanField(read_only=True)
-
-    class Meta:
-        model = Product
-        fields = ('id', 'name', 'price', 'original_price', 'discount',
-                  'category', 'image_url', 'rating', 'in_stock', 'is_active')
-
-    def get_discount(self, obj):
-        return obj.discount_percentage
-
-#----------------------------------------------------------------
-#                   Brand Serializer
-#----------------------------------------------------------------
-class BrandSerializer(serializers.ModelSerializer):
-    product_count = serializers.SerializerMethodField()
-
-    class Meta:
-        model = Brand
-        fields = ('id', 'name', 'popularity', 'rating', 'likes', 'dislikes', 'product_count')
-
-    def get_product_count(self, obj):
-        return obj.products.count()
-
-#----------------------------------------------------------------
-#                   Product Detail Serializer
-#----------------------------------------------------------------
-class ProductDetailSerializer(serializers.ModelSerializer):
-    reviews = ReviewSerializer(many=True, read_only=True, required=False, default=list)
-    category = CategorySerializer(read_only=True)
-    brand = BrandSerializer(read_only=True)
-    shop = ShopOwnerSerializer(read_only=True)
-    discount = serializers.SerializerMethodField()
-    rating = serializers.FloatField(read_only=True, required=False)
-    in_stock = serializers.BooleanField(read_only=True, required=False)
-    category_id = serializers.UUIDField(write_only=True, required=True, allow_null=False)
-    brand_id = serializers.UUIDField(write_only=True, required=True, allow_null=False)
-
-    class Meta:
-        model = Product
-        fields = (
-            'id', 'name', 'description', 'price', 'original_price',
-            'discount', 'category', 'category_id', 'brand', 'brand_id', 'shop', 'image_url', 'in_stock', 'rating', 'is_active', 'created_at',
-            'reviews', 'video_url', 'release_date', 'likes', 'dislikes', 'neutrals',
-            'views', 'is_banned'
-        )
-        extra_kwargs = {
-            'brand': {'required': False},
-            'rating': {'read_only': True},
-            'original_price': {'required': False},
-            'is_active': {'default': True}
-        }
-
-    def get_discount(self, obj):
-        return obj.discount_percentage
-
-    def to_internal_value(self, data):
-        # دعم brand_id مباشرة من البيانات
-        ret = super().to_internal_value(data)
-        brand_id = data.get('brand_id')
-        if brand_id:
-            ret['brand_id'] = brand_id
-        return ret
-
-    def create(self, validated_data):
-        # استخراج category_id وربطه فعليًا
-        category_id = validated_data.pop('category_id', None)
-        if not category_id:
-            raise serializers.ValidationError({'category_id': 'هذا الحقل مطلوب.'})
-        try:
-            category = Category.objects.get(pk=category_id)
-        except Category.DoesNotExist:
-            raise serializers.ValidationError({'category_id': 'التصنيف غير موجود.'})
-        validated_data['category'] = category
-
-        # استخراج brand_id وربطه فعليًا
-        brand_id = validated_data.pop('brand_id', None)
-        if not brand_id:
-            raise serializers.ValidationError({'brand_id': 'هذا الحقل مطلوب.'})
-        from core.models import Brand
-        try:
-            brand = Brand.objects.get(pk=brand_id)
-        except Brand.DoesNotExist:
-            raise serializers.ValidationError({'brand_id': 'البراند غير موجود.'})
-        validated_data['brand'] = brand
-
-        # القيم الافتراضية
-        if 'rating' not in validated_data:
-            validated_data['rating'] = 0
-        if 'in_stock' not in validated_data:
-            validated_data['in_stock'] = True
-        return super().create(validated_data)
-
-#----------------------------------------------------------------
-#                   Shop Serializer
-#----------------------------------------------------------------
-class ShopSerializer(serializers.ModelSerializer):
-    owner = serializers.SerializerMethodField()
-    product_count = serializers.SerializerMethodField()
-    completion_percentage = serializers.SerializerMethodField()
-
-    class Meta:
-        model = Shop
-        fields = ('id', 'name', 'address', 'description', 'logo', 'banner', 'url',
-                 'phone', 'email', 'social_media', 'owner', 'product_count',
-                 'completion_percentage')
-        read_only_fields = ('id', 'owner', 'product_count', 'completion_percentage')
-
-    def get_owner(self, obj):
-        if obj.owner and obj.owner.user:
-            return {
-                'id': str(obj.owner.user.id),  # يجب أن يكون معرف المستخدم
-                'username': obj.owner.user.username,
-                'email': obj.owner.user.email,
-                'is_active': obj.owner.user.is_active
-            }
-        return None
-
-    def get_product_count(self, obj):
-        return obj.products.count()
-
-    def get_completion_percentage(self, obj):
-        required_fields = ['name', 'address', 'description', 'logo', 'url', 'phone', 'email']
-        completed = sum(1 for field in required_fields if getattr(obj, field))
-        return int((completed / len(required_fields)) * 100)
-
-
-#----------------------------------------------------------------
-#                   ProductList Serializer
-#----------------------------------------------------------------
-class ProductListSerializer(serializers.ModelSerializer):
-
-    category = CategorySerializer(read_only=True)
-    discount = serializers.SerializerMethodField()
-    rating = serializers.FloatField(read_only=True)
-    in_stock = serializers.BooleanField(read_only=True)
     shop_name = serializers.SerializerMethodField()
 
     class Meta:
         model = Product
         fields = ('id', 'name', 'price', 'original_price', 'discount',
                   'category', 'image_url', 'rating', 'likes','dislikes',
-                  'neutrals', 'is_active', 'shop_name', 'in_stock', 'views')
+                  'neutrals', 'is_active', 'shop_name', 'views')
 
     def get_discount(self, obj):
         if obj.original_price and obj.price and obj.original_price > obj.price:
@@ -225,7 +90,6 @@ class ProductDetailSerializer(serializers.ModelSerializer):
     shop = ShopOwnerSerializer(read_only=True)
     discount = serializers.SerializerMethodField()
     rating = serializers.FloatField(read_only=True, required=False)
-    in_stock = serializers.BooleanField(read_only=True, required=False)
     category_id = serializers.UUIDField(write_only=True, required=True, allow_null=False)
     brand_id = serializers.UUIDField(write_only=True, required=True, allow_null=False)
 
@@ -237,7 +101,7 @@ class ProductDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = ('id', 'name', 'description', 'price', 'original_price',
-                  'discount', 'category', 'category_id', 'brand', 'brand_id', 'shop', 'image_url', 'in_stock', 'rating', 'is_active', 'created_at',
+                  'discount', 'category', 'category_id', 'brand', 'brand_id', 'shop', 'image_url', 'rating', 'is_active', 'created_at',
                   'reviews', 'video_url', 'release_date', 'likes', 'dislikes', 'neutrals',
                   'views', 'is_banned')
         extra_kwargs = {
@@ -285,8 +149,150 @@ class ProductDetailSerializer(serializers.ModelSerializer):
         # القيم الافتراضية
         if 'rating' not in validated_data:
             validated_data['rating'] = 0
-        if 'in_stock' not in validated_data:
-            validated_data['in_stock'] = True
+        return super().create(validated_data)
+
+#----------------------------------------------------------------
+#                   Shop Serializer
+#----------------------------------------------------------------
+class ShopSerializer(serializers.ModelSerializer):
+    owner = serializers.SerializerMethodField()
+    product_count = serializers.SerializerMethodField()
+    completion_percentage = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Shop
+        fields = ('id', 'name', 'address', 'description', 'logo', 'banner', 'url',
+                 'phone', 'email', 'social_media', 'owner', 'product_count',
+                 'completion_percentage')
+        read_only_fields = ('id', 'owner', 'product_count', 'completion_percentage')
+
+    def get_owner(self, obj):
+        if obj.owner and obj.owner.user:
+            return {
+                'id': str(obj.owner.user.id),  # يجب أن يكون معرف المستخدم
+                'username': obj.owner.user.username,
+                'email': obj.owner.user.email,
+                'is_active': obj.owner.user.is_active
+            }
+        return None
+
+    def get_product_count(self, obj):
+        return obj.products.count()
+
+    def get_completion_percentage(self, obj):
+        required_fields = ['name', 'address', 'description', 'logo', 'url', 'phone', 'email']
+        completed = sum(1 for field in required_fields if getattr(obj, field))
+        return int((completed / len(required_fields)) * 100)
+
+
+#----------------------------------------------------------------
+#                   ProductList Serializer
+#----------------------------------------------------------------
+class ProductListSerializer(serializers.ModelSerializer):
+
+    category = CategorySerializer(read_only=True)
+    discount = serializers.SerializerMethodField()
+    rating = serializers.FloatField(read_only=True)
+    shop_name = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Product
+        fields = ('id', 'name', 'price', 'original_price', 'discount',
+                  'category', 'image_url', 'rating', 'likes','dislikes',
+                  'neutrals', 'is_active', 'shop_name', 'views')
+
+    def get_discount(self, obj):
+        if obj.original_price and obj.price and obj.original_price > obj.price:
+            return int(((obj.original_price - obj.price) / obj.original_price) * 100)
+        return 0
+
+    def get_shop_name(self, obj):
+        if obj.shop:
+            return obj.shop.name
+        return None
+
+#----------------------------------------------------------------
+#                   Brand Serializer
+#----------------------------------------------------------------
+class BrandSerializer(serializers.ModelSerializer):
+    product_count = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Brand
+        fields = ('id', 'name', 'popularity', 'rating', 'likes', 'dislikes', 'product_count')
+
+    def get_product_count(self, obj):
+        return obj.products.count()
+
+#----------------------------------------------------------------
+#                   Product Detail Serializer
+#----------------------------------------------------------------
+class ProductDetailSerializer(serializers.ModelSerializer):
+    reviews = ReviewSerializer(many=True, read_only=True, required=False, default=list)
+    category = CategorySerializer(read_only=True)
+    brand = BrandSerializer(read_only=True)
+    shop = ShopOwnerSerializer(read_only=True)
+    discount = serializers.SerializerMethodField()
+    rating = serializers.FloatField(read_only=True, required=False)
+    category_id = serializers.UUIDField(write_only=True, required=True, allow_null=False)
+    brand_id = serializers.UUIDField(write_only=True, required=True, allow_null=False)
+
+    def get_shop_name(self, obj):
+        if obj.shop:
+            return obj.shop.name
+        return None
+
+    class Meta:
+        model = Product
+        fields = ('id', 'name', 'description', 'price', 'original_price',
+                  'discount', 'category', 'category_id', 'brand', 'brand_id', 'shop', 'image_url', 'rating', 'is_active', 'created_at',
+                  'reviews', 'video_url', 'release_date', 'likes', 'dislikes', 'neutrals',
+                  'views', 'is_banned')
+        extra_kwargs = {
+            'brand': {'required': False},
+            'rating': {'read_only': True},  # جعل حقل التقييم للقراءة فقط
+            'original_price': {'required': False},
+            'is_active': {'default': True}
+        }
+
+    def get_discount(self, obj):
+        if obj.original_price and obj.price and obj.original_price > obj.price:
+            return int(((obj.original_price - obj.price) / obj.original_price) * 100)
+        return 0
+
+    def to_internal_value(self, data):
+        # دعم brand_id مباشرة من البيانات
+        ret = super().to_internal_value(data)
+        brand_id = data.get('brand_id')
+        if brand_id:
+            ret['brand_id'] = brand_id
+        return ret
+
+    def create(self, validated_data):
+        # استخراج category_id وربطه فعليًا
+        category_id = validated_data.pop('category_id', None)
+        if not category_id:
+            raise serializers.ValidationError({'category_id': 'هذا الحقل مطلوب.'})
+        try:
+            category = Category.objects.get(pk=category_id)
+        except Category.DoesNotExist:
+            raise serializers.ValidationError({'category_id': 'التصنيف غير موجود.'})
+        validated_data['category'] = category
+
+        # استخراج brand_id وربطه فعليًا
+        brand_id = validated_data.pop('brand_id', None)
+        if not brand_id:
+            raise serializers.ValidationError({'brand_id': 'هذا الحقل مطلوب.'})
+        from core.models import Brand
+        try:
+            brand = Brand.objects.get(pk=brand_id)
+        except Brand.DoesNotExist:
+            raise serializers.ValidationError({'brand_id': 'البراند غير موجود.'})
+        validated_data['brand'] = brand
+
+        # القيم الافتراضية
+        if 'rating' not in validated_data:
+            validated_data['rating'] = 0
         return super().create(validated_data)
 
 #----------------------------------------------------------------
