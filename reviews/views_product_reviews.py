0f37b0faf36dbcdd2ac993505171d374447d2f98
Mugahed Ahmed Batahan <ttt.ppp.sss.77@gmail.com>
2025-06-29 17:06:06 +0300

    Update views_product_reviews.py
    Fix review creation by directly saving product and user in serializer

--- reviews/views_product_reviews.py
+++ reviews/views_product_reviews.py
@@ -38,12 +38,9 @@ class ProductReviewsView(APIView):
         
         # Crear una nueva reseña
         data = request.data.copy()
-        data['product'] = product.id
-        data['user'] = request.user.id
-        
         serializer = ReviewSerializer(data=data)
         if serializer.is_valid():
-            serializer.save()
+            serializer.save(product=product, user=request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
